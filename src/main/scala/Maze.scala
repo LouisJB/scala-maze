b6884b78a54c2e1f27f8115843e087d775362c23
Louis J B <chillipower.uk@gmail.com>
2024-10-18 22:04:53 -0400

    improvements, add double-buffering and other tidy-ups

--- src/main/scala/Maze.scala
+++ src/main/scala/Maze.scala
@@ -3,20 +3,40 @@
  */
 package maze
 
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Graphics
-import java.awt.Rectangle
-import java.awt.event.KeyEvent
-import java.awt.event.KeyListener
 import scala.swing.BorderPanel
 import scala.swing.Frame
 import scala.swing.MainFrame
 import scala.swing.Panel
 import scala.swing.event.KeyPressed
 import scala.util.Random
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.Rectangle
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.awt.image.BufferedImage
+import java.awt.RenderingHints
+import java.awt.Graphics2D
+import java.awt.BasicStroke
 
 
+object MazeModel {
+  val HEIGHT = 25
+  val WIDTH = 40
+}
+object MazeConsts {
+  val frameBorder = 25
+  val frameHeight = 1000 - frameBorder
+  val frameWidth = frameHeight
+
+  val delayTimeMs = 4000
+  val createDelayMs = 10
+  val solveDelayMs = 20
+  val delayStopMs = 2000
+
+  val lineThickness = 2.0f
+}
 object Direction extends Enumeration {
   type Direction = Value
     val North, South, East, West = Value
@@ -39,21 +59,6 @@ object Breadcrumb extends Enumeration {
 
 import Direction._
 import Breadcrumb._
-
-object MazeModel {
-  val HEIGHT = 10
-  val WIDTH = 10
-}
-object MazeConsts {
-  val frameBorder = 25
-  val frameHeight = 800 - frameBorder
-  val frameWidth = frameHeight
-
-  val delayTimeMs = 4000
-  val createDelayMs = 50
-  val solveDelayMs = 100
-  val delayStopMs = 2000
-}
 import MazeModel._
 import MazeConsts._
 import Audio._
@@ -65,6 +70,8 @@ case class Sounds(audioSynth: Option[AudioSynth]) {
     audioSynth.foreach(_.sine(600, 25))
   def blip() =
     audioSynth.foreach(_.blip(200, 400, 20, 200))
+  def blipDown() =
+    audioSynth.foreach(_.blip(400, 100, 20, 100))
   def sweepUp() =
     audioSynth.foreach(_.sweep(200, 2000, 5, 2000))
   def sweepDown() =
@@ -81,11 +88,13 @@ class MazeModel(sounds: Sounds) {
   import sounds._
   var cells = Array.tabulate(WIDTH, HEIGHT)((i, j) => Cell(i, j))
 
+  val start = new Point(0, 0)
+  // cell width-1,height-1 is the exit, open the south wall to highlight this
+  val exit = new Point(WIDTH - 1, HEIGHT - 1)
+  val startCell = cellAt(start)
+
   // generate maze
   def generateMaze(update: => Unit): Unit = {
-    val exit = new Point(WIDTH - 1, HEIGHT - 1)
-    val start = new Point(0, 0)
-
     // 1. Start at a particular cell, call it the "exit"
     // 2. Mark the current cell as visited, and get a list of all its neighbors.
     //      For each neighbor, starting with a randomly selected neighbor:
@@ -94,11 +103,9 @@ class MazeModel(sounds: Sounds) {
     //        and then recurse with that neighbor as the current cell.
 
     // cell 0,0 is the start, open the north wall to highlight this
-    val startCell = cells(0)(0)
     startCell.clear(North)
 
-    // cell width-1,height-1 is the exit, open the south wall to highlight this
-    val endCell = cells(exit.x)(exit.y)
+    val endCell = cellAt(exit)
     endCell.clear(South)
 
     // recursively process the next cell
@@ -131,16 +138,12 @@ class MazeModel(sounds: Sounds) {
   // find the maze solution using dfs from the start node until the end
   // node is located
   def solveMazeDFS(update: => Unit): Unit = {
-    val start = new Point(0, 0)
-    val exit = new Point(WIDTH - 1, HEIGHT - 1)
-
-    val startCell = cells(0)(0)
     startCell.clear(North)
     try {
       def doNextCell(c: Cell): Unit = {
         c.visited = true
         c.trail = Forward
-
+        if (c.getDirections().isEmpty) blipDown()
         c.getDirections().foreach { dir =>
           getCell(c, dir).foreach { n =>
             if (n.visited != true) {
@@ -149,7 +152,7 @@ class MazeModel(sounds: Sounds) {
               n.trail = Forward
               update
               beep()
-              if (n.i == exit.x && n.j == exit.y) throw new Exception("Done")
+              if (n.isAt(exit)) throw new Exception("Done")
               silence(solveDelayMs)
               doNextCell(n)
               n.trail = Backward
@@ -170,11 +173,6 @@ class MazeModel(sounds: Sounds) {
   // find the maze solution using dfs from the start node until the end node is located
   def solveMazeBFS(update: => Unit): Unit = {
     val q = new collection.mutable.Queue[Cell]
-
-    val start = new Point(0, 0)
-    val exit = new Point(WIDTH - 1, HEIGHT - 1)
-
-    val startCell = cells(0)(0)
     startCell.clear(North)
 
     q.enqueue(startCell)
@@ -191,7 +189,7 @@ class MazeModel(sounds: Sounds) {
           update
           beep()
 
-          if (c.i == exit.x && c.j == exit.y) throw new Exception("Done")
+          if (c.isAt(exit)) throw new Exception("Done")
 
           c.getDirections().foreach { dir =>
             getCell(c, dir).foreach { n =>
@@ -216,12 +214,10 @@ class MazeModel(sounds: Sounds) {
   // from the exit recurse over the trail of predecessors,
   // marking with a forward trail
   def showSolution(update: => Unit): Unit = {
-    val exit = new Point(WIDTH - 1, HEIGHT - 1)
-
     println(">>showSolution")
 
     // Stream.iterate(cells(exit.n)(exit.y))(_.pi).takeWhile(_ != null)
-    val cell: Option[Cell] = Some(cells(exit.x)(exit.y))
+    val cell: Option[Cell] = Some(cellAt(exit))
     LazyList.iterate(cell)(_.flatMap(_.pi)).takeWhile(_ != None).foreach {
       case Some(n) =>
         n.trail = Forward
@@ -240,6 +236,8 @@ class MazeModel(sounds: Sounds) {
     case West   => if (c.i > 0) Some(cells(c.i-1)(c.j)) else None
   }
 
+  def cellAt(p: Point) = cells(p.x)(p.y)
+
   def mapCells(f: Cell => Cell) = 
     cells.foreach(_.foreach(f))
 
@@ -255,11 +253,23 @@ class MazeModel(sounds: Sounds) {
   }
 }
 
-// a scala.swing.Panel, override paint(Graphics2D) to paint each of the cells
+// a scala.swing.Panel, override paintComponent(Graphics2D) to paint each of the cells
 class MazePanel(mm: MazeModel) extends Panel {
   override def paintComponent(g: java.awt.Graphics2D) = {
-    super.paintComponent(g)
-    mm.mapCells(c => {c.draw(g); c })
+    val clipBounds = g.getClipBounds()
+    // set up an offscreen screen buffer to double-buffer render on to
+    val buffImg = new BufferedImage(clipBounds.width, clipBounds.height, BufferedImage.TYPE_INT_RGB)
+    val g2d = buffImg.createGraphics()
+    g2d.setClip(clipBounds)
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    super.paintComponent(g2d)
+
+    mm.mapCells(c => {c.draw(g2d); c })
+
+    // draw out the offline screen buffer to the displayed graphics screen, to display it
+    val g2dr = g.asInstanceOf[Graphics2D]
+    g2dr.drawImage(buffImg, 0, 0, null)
+    g2d.dispose()
   }
 }
 
@@ -338,11 +348,11 @@ object Maze {
 	    args(0).trim.toLowerCase == "nosound"
     else false
     val sizeDims = new java.awt.Dimension(frameWidth + frameBorder, frameHeight + frameBorder)
-    val maze = new Maze(noSound)
     val frame = new MainFrame()
     frame.preferredSize = new java.awt.Dimension(sizeDims)
     frame.bounds = new Rectangle(0, 0, sizeDims.width, sizeDims.height)
-    frame.title = "Simple Maze Demo v0.1"
+    frame.title = "Simple Maze Demo v0.2"
+    val maze = new Maze(noSound)
     maze.mazePanel.background_= = new Color(124, 255, 64)
     frame.contents = new BorderPanel { add(maze.mazePanel, BorderPanel.Position.Center) }
     frame.peer.addKeyListener(mkKeyListener(frame))
@@ -371,7 +381,7 @@ object Maze {
 }
 
 object Cell {
-  val cellBorder = 10
+  val cellBorder = 5
   val origin = Cell(0, 0)
 }
 
@@ -390,34 +400,34 @@ case class Cell(i: Int, j: Int) {
   var trail: Breadcrumb = Clear
   var gen: Int = 0
 
-  def draw(g: Graphics): Unit = {
-    val bounds = g.getClipBounds()
+  def draw(g2: Graphics2D): Unit = {
+    val bounds = g2.getClipBounds()
     val size = Math.min(bounds.width / WIDTH, bounds.height / HEIGHT)
     val x = bounds.x + i * size
     val y = bounds.y + j * size
 
     def fillCell() =
-      g.fillRect(x + cellBorder, y + cellBorder, size - cellBorder * 2, size - cellBorder * 2)
+      g2.fillRect(x + cellBorder, y + cellBorder, size - cellBorder * 2, size - cellBorder * 2)
     
     trail match {
       case Forward => {
         val c = getColour(gen)
-        g.setColor(c)
+        g2.setColor(c)
         fillCell()
       }
       case Backward => {
-        g.setColor(Color.BLUE)
+        g2.setColor(Color.BLUE)
         fillCell()
       }
       case _ => // leave blank/clear
     }
 
-    g.setColor(Color.BLACK)
-
-    if (north) g.drawLine(x, y, x + size, y)
-    if (south) g.drawLine(x, y + size, x + size, y + size)
-    if (east) g.drawLine(x + size, y, x + size, y + size)
-    if (west) g.drawLine(x, y, x, y + size)
+    g2.setColor(Color.BLACK)
+    g2.setStroke(new BasicStroke(lineThickness))
+    if (north) g2.drawLine(x, y, x + size, y)
+    if (south) g2.drawLine(x, y + size, x + size, y + size)
+    if (east) g2.drawLine(x + size, y, x + size, y + size)
+    if (west) g2.drawLine(x, y, x, y + size)
   }
 
   def clear(dir : Direction) = dirs -= dir
@@ -447,6 +457,9 @@ case class Cell(i: Int, j: Int) {
     case 6 => Color.BLUE
     case _ => Color.MAGENTA
   }
+
+  def isAt(p: Point) =
+    i == p.x && j == p.y
 }
 
 // represent a 2d point
